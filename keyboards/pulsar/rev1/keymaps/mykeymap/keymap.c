0439812b717392ae3bc9dac24da1868b10549c4b
shunichi fujiwara <sh1@dream.jp>
2020-08-30 03:11:42 +0900

    4 mode

--- keyboards/pulsar/rev1/keymaps/mykeymap/keymap.c
+++ keyboards/pulsar/rev1/keymaps/mykeymap/keymap.c
@@ -24,8 +24,11 @@
 
 enum {
   _BASE = 0,
-  _PRO = 1,
-  _LED  = 2
+  _CSPIP =1,
+  _PS = 2, 
+  _PRO = 3,
+  _FUNC = 4,
+  _LED  = 5
 };
 
 enum {
@@ -39,15 +42,33 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT( /* clip studio paint */
-    KC_LSHIFT, KC_ESCAPE,    KC_F1,     C(KC_F16), \
-    KC_LCTRL,  KC_SPACE,    KC_LALT,     A(KC_BTN2), \
-                                           A(S(KC_BTN2)), \
-    JP_LBRC,   XXXXXXX, TO(_PRO), XXXXXXX, JP_RBRC \
+    KC_LSHIFT, KC_ESCAPE,    KC_F1,     KC_F2, \
+    KC_LCTRL,  KC_SPACE,    KC_LALT,     A(C(KC_Z)), \
+                                           S(C(KC_Z)), \
+    KC_PGUP,   XXXXXXX, TO(_CSPIP), XXXXXXX, KC_PGDOWN \
+  ),
+  [_CSPIP] = LAYOUT( /* clip studio paint ipad*/
+    KC_LSHIFT, KC_ESCAPE,    KC_F1,     KC_F2, \
+    KC_LGUI,  KC_SPACE,    KC_LALT,     A(LGUI(KC_Z)), \
+                                           S(LGUI(KC_Z)), \
+    KC_PGUP,   XXXXXXX, TO(_PS), XXXXXXX, KC_PGDOWN \
+  ),
+  [_PS] = LAYOUT( /* photoshop */
+    KC_LSHIFT, A(KC_BTN2),    A(S(KC_BTN2)),     KC_F1, \
+    KC_LCTRL,  KC_SPACE,    KC_LALT,     A(C(KC_Z)), \
+                                           S(C(KC_Z)), \
+    KC_PGUP,   XXXXXXX, TO(_PRO), XXXXXXX, KC_PGDOWN \
   ),
   [_PRO] = LAYOUT( /* procreate */
-    KC_LSHIFT, KC_U,    KC_F1,     C(KC_F16), \
-    KC_LCTRL,  KC_SPACE,    KC_LALT,     A(KC_BTN2), \
-                                           A(S(KC_BTN2)), \
+    KC_LSHIFT, KC_U,    KC_F1,     KC_F2, \
+    KC_LGUI,  KC_SPACE,    KC_LALT,     A(LGUI(KC_Z)), \
+                                           S(LGUI(KC_Z)), \
+    JP_LBRC,   XXXXXXX, TO(_FUNC), XXXXXXX, JP_RBRC \
+  ),
+  [_FUNC] = LAYOUT( /* func*/
+    KC_F13, KC_F14,    KC_F15,     KC_F19, \
+    KC_F16,  KC_F17,    KC_F18,     KC_F20, \
+                                           KC_F21, \
     JP_LBRC,   XXXXXXX, TO(_LED), XXXXXXX, JP_RBRC \
   ),
   [_LED] = LAYOUT( /* Base */
@@ -78,8 +99,17 @@ void oled_task_user(void) {
     case _BASE:
       oled_write_P(PSTR("Layer\n:BASE\n"), false);
       break;
+    case _CSPIP:
+      oled_write_P(PSTR("Layer\n:CSP IPAD\n"), false);
+      break;
+    case _PS:
+      oled_write_P(PSTR("Layer\n:PS\n"), false);
+      break;
     case _PRO:
       oled_write_P(PSTR("Layer\n:PRO\n"), false);
+      break;
+    case _FUNC:
+      oled_write_P(PSTR("Layer\n:FUNC\n"), false);
       break;
     case _LED:
       oled_write_P(PSTR("Layer\n:LED\n"), false);
